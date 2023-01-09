a4f5fdbaf2df45c5bb68c9bc6537634af6f84850
Andreu6 <suarezbellavistaandreu@gmail.com>
2023-01-09 19:25:49 +0100

    Base

---
+++ Base de ejercicio - Routes, Guards y Services (CatMania)/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//TODO: Definir las ruta/s que correspondan
+const routes: Routes = [
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
